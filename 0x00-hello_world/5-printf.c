c89e45e9d30b74ed8c134bf96c5e37f72b3687a1
lamiya-Mohamed <lamiyamohamed2972019@gmail.com>
2023-03-17 11:50:31 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A Program that print a line using the printf
+ unction
+ * Return: 0 (Sucess)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome
+is a piece of art,\n")
+return(0);
+}        
